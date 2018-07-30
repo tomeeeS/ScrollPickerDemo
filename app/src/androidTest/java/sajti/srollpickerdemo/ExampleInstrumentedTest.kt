5a21b11dcd4c3d22f0b15614d907d9958a457822
tomeeeS <sajtitamas1@hotmail.com>
2018-07-30 19:11:21 +0200

    commit

--- app/src/androidTest/java/sajti/srollpickerdemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/sajti/srollpickerdemo/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
-package com.example.sajti.srollpickerdemo
+package sajti.srollpickerdemo
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
---
+++ app/src/main/java/sajti/srollpickerdemo/MainActivity.kt
@@ -0,0 +1,70 @@
+package sajti.srollpickerdemo
+
+import android.content.Intent
+import android.databinding.DataBindingUtil
+import android.databinding.ObservableBoolean
+import android.databinding.ObservableInt
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import sajti.scroll_picker.ScrollPicker
+import sajti.srollpickerdemo.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var scrollPicker: ScrollPicker
+
+    private val LAYOUT = R.layout.activity_main
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val pickerValue = ObservableInt( 5 )
+        val isEnabled = ObservableBoolean( true )
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView( this, LAYOUT )
+        binding.mainContent!!.setPickerValue( pickerValue )
+        binding.mainContent.setIsEnabled( isEnabled )
+        setSupportActionBar(toolbar)
+
+        scrollPicker = findViewById( R.id.picker )
+        scrollPicker.setShownItemCount( 4 )
+        val colors = getResources().getStringArray( R.array.colors )
+        scrollPicker.setList( colors.toCollection( ArrayList() ) )
+        scrollPicker.setEnabled( true )
+
+        fab.setOnClickListener { _ ->
+            run {
+                pickerValue.set( 2 )
+            }
+        }
+        fab2.setOnClickListener { _ ->
+            run {
+                val setEnabled = !isEnabled.get()
+                isEnabled.set( setEnabled )
+            }
+        }
+    }
+
+    //region optionsMenu
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate( R.menu.menu_main, menu )
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when(item.itemId) {
+            R.id.action_settings ->
+                startActivity( Intent( this, IntListActivity::class.java ) )
+            else -> super.onOptionsItemSelected( item )
+        }
+        return true
+    }
+    //endregion optionsMenu
+}
