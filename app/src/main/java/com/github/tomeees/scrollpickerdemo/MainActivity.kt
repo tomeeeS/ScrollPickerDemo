75e94003479e859aa001a569037eb795a6b9f7f5
tomeeeS <sajtitamas1@hotmail.com>
2018-08-22 17:00:42 +0200

    removed navigation so a non-alpha version of android studio won't be necessary to try out this demo project

--- app/src/main/java/com/github/tomeees/scrollpickerdemo/MainActivity.kt
+++ app/src/main/java/com/github/tomeees/scrollpickerdemo/MainActivity.kt
@@ -2,30 +2,16 @@ package com.github.tomeees.scrollpickerdemo
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
 import com.github.tomeees.scrollpickerdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        drawerLayout = binding.drawerLayout
-
-        val navController = Navigation.findNavController(this, R.id.nav_fragment)
-
         // Set up ActionBar
         setSupportActionBar(binding.toolbar)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-
-        // Set up navigation menu
-        binding.navigationView.setupWithNavController(navController)
     }
 }
