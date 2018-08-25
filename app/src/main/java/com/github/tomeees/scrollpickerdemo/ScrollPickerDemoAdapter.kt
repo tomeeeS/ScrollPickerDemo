e70252af3ae6754a6cf37a239ba94f9be70abd61
tomeeeS <sajtitamas1@hotmail.com>
2018-08-25 21:45:42 +0200

    UI finished

--- app/src/main/java/com/github/tomeees/scrollpickerdemo/ScrollPickerDemoAdapter.kt
+++ app/src/main/java/com/github/tomeees/scrollpickerdemo/ScrollPickerDemoAdapter.kt
@@ -2,19 +2,22 @@ package com.github.tomeees.scrollpickerdemo
 
 import android.content.Context
 import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
 import android.graphics.Color
 import android.support.v4.view.PagerAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.github.tomeees.scrollpicker.ScrollPicker
+import com.github.tomeees.scrollpicker.SelectorStyle
 import com.github.tomeees.scrollpickerdemo.databinding.ViewPagerPage1Binding
+import com.github.tomeees.scrollpickerdemo.databinding.ViewPagerPage2Binding
 import java.util.*
 
 class ScrollPickerDemoAdapter( val context: Context, val scrollPicker: ScrollPicker, val mainFragmentViewModel: MainFragmentViewModel) : PagerAdapter() {
 
     val LAYOUT_PAGE_1 = R.layout.view_pager_page_1
-    val LAYOUT_PAGE_2 = R.layout.view_pager_page_1
+    val LAYOUT_PAGE_2 = R.layout.view_pager_page_2
 
     val COLOR_RANDOM_INT_BOUND: Int = 256
     val COLOR_MAX_ALPHA: Int = 255
@@ -24,21 +27,46 @@ class ScrollPickerDemoAdapter( val context: Context, val scrollPicker: ScrollPic
     }
 
     override fun getCount(): Int {
-        return 1
+        return 2
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return (position + 1).toString()
     }
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val binding = DataBindingUtil.inflate<ViewPagerPage1Binding>( LayoutInflater.from( context ),
-                if( position == 1 ) LAYOUT_PAGE_1 else LAYOUT_PAGE_2,
+        val binding: ViewDataBinding = when( position ) {
+            0    -> setPage1Binding(container)
+            else -> setPage2Binding(container)
+        }
+        container.addView( binding.root )
+        return binding.root
+    }
+
+    fun setPage2Binding( container: ViewGroup): ViewDataBinding {
+        return DataBindingUtil.inflate<ViewPagerPage2Binding>(LayoutInflater.from(context),
+                LAYOUT_PAGE_2,
                 container,
                 false).apply {
-            setViewModel( mainFragmentViewModel )
+            setViewModel(mainFragmentViewModel)
+            selectorStyle1.setOnClickListener { _ -> scrollPicker.setSelectorStyle( SelectorStyle.RECTANGLE_FILLED ) }
+            selectorStyle2.setOnClickListener { _ -> scrollPicker.setSelectorStyle( SelectorStyle.RECTANGLE ) }
+            selectorStyle3.setOnClickListener { _ -> scrollPicker.setSelectorStyle( SelectorStyle.CLASSIC ) }
+            selectedItemTextColor.setOnClickListener { _ -> scrollPicker.setSelectedTextColor(getRandomColor()) }
+            setSelectedTextSize.setOnClickListener { _ -> scrollPicker.setSelectedTextSize(24f) }
+        }
+    }
+
+    fun setPage1Binding(container: ViewGroup): ViewDataBinding {
+        return DataBindingUtil.inflate<ViewPagerPage1Binding>(LayoutInflater.from(context),
+                LAYOUT_PAGE_1,
+                container,
+                false).apply {
+            setViewModel(mainFragmentViewModel)
             selectorColor.setOnClickListener { _ -> scrollPicker.setSelectorColor(getRandomColor()) }
             textColor.setOnClickListener { _ -> scrollPicker.setTextColor(getRandomColor()) }
             setTextSize.setOnClickListener { _ -> scrollPicker.setTextSize(20f) }
         }
-        container.addView( binding.root )
-        return binding.root
     }
 
     fun getRandomColor(): Int {
