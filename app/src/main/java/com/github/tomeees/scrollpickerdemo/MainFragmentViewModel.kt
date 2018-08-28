ad81ced33132f8f27f7605e739600522c46abee1
tomeeeS <sajtitamas1@hotmail.com>
2018-08-28 10:35:07 +0200

    added date list, diversified viewmodel collections

--- app/src/main/java/com/github/tomeees/scrollpickerdemo/MainFragmentViewModel.kt
+++ app/src/main/java/com/github/tomeees/scrollpickerdemo/MainFragmentViewModel.kt
@@ -4,19 +4,19 @@ import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
 import android.databinding.ObservableInt
+import java.util.*
 
 class MainFragmentViewModel : ViewModel() {
 
-    val DEFAULT_STRING_PICKER_VALUE: Int = 1
-
-    val numbers: ArrayList< Int > = arrayListOf(-5, 3, -15, 7, 4, -50, 2, 70)
-    val colors: ArrayList< String > = arrayListOf("red", "green", "blue", "yellow", "purple", "white", "llama color",
+    val numbers: List< Int > = listOf(-5, 3, -15, 7, 4, -50, 2, 70)
+    val dates: ArrayList< Date > = arrayListOf( Date(), Date(), Date() )
+    val colors: Set< String > = setOf("red", "green", "blue", "yellow", "purple", "white", "llama color",
             "teal", "black", "pink", "orange", "gold", "aquamarine", "brown", "gray")
     var pickerValue: ObservableInt = ObservableInt()
     var isEnabled: ObservableBoolean = ObservableBoolean(true)
     var shownList: ObservableField< Collection< Any > > = ObservableField()
 
-    private var showNumbers = true
+    private var whichListToShow = 0
 
     init {
         initList()
@@ -27,17 +27,16 @@ class MainFragmentViewModel : ViewModel() {
     }
 
     fun changeList() {
-        showNumbers = !showNumbers
+        whichListToShow = if( whichListToShow == 2 ) 0 else whichListToShow + 1
         initList()
     }
 
     fun initList() {
-        if (showNumbers) {
-            shownList.set(numbers)
-            pickerValue.set(numbers[ 1 ])
-        } else {
-            shownList.set(colors)
-            pickerValue.set(DEFAULT_STRING_PICKER_VALUE)
-        }
+        shownList.set( when( whichListToShow ) {
+            0 -> numbers
+            1 -> colors
+            else -> dates
+        } )
+        pickerValue.set( 2 )
     }
 }
